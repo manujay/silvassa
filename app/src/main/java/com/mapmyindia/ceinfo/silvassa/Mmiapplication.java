1f914b35b7775134100c8ceca2b36536bb2bd0cc
Manujay K. Yadav <manujay1989@gmail.com>
2017-03-28 18:05:25 +0530

    check zonId for Sync Search

--- app/src/main/java/com/mapmyindia/ceinfo/silvassa/Mmiapplication.java
+++ app/src/main/java/com/mapmyindia/ceinfo/silvassa/Mmiapplication.java
@@ -2,8 +2,11 @@ package com.mapmyindia.ceinfo.silvassa;
 
 import android.app.Application;
 
+import com.orhanobut.logger.LogAdapter;
 import com.orhanobut.logger.LogLevel;
 import com.orhanobut.logger.Logger;
+
+import java.util.Locale;
 
 /**
  * Created by ceinfo on 28-03-2017.
@@ -22,7 +25,42 @@ public class Mmiapplication extends Application {
                 .methodCount(3)                 // default 2
 //                .hideThreadInfo()               // default shown
                 .logLevel(LogLevel.FULL)        // default LogLevel.FULL
-                .methodOffset(2);                // default 0
-//                .logAdapter(new AndroidLogAdapter()); //default AndroidLogAdapter
+                .methodOffset(2)            // default 0
+                .logAdapter(new AndroidLogAdapter()); //default AndroidLogAdapter
+    }
+
+
+    public class AndroidLogAdapter implements LogAdapter {
+
+        @Override
+        public void d(String tag, String message) {
+            System.out.println(String.format(Locale.getDefault(), "@" + tag + ": %s ", message));
+        }
+
+        @Override
+        public void e(String tag, String message) {
+            System.out.println(String.format(Locale.getDefault(), "@" + tag + ": %s ", message));
+        }
+
+        @Override
+        public void w(String tag, String message) {
+            System.out.println(String.format(Locale.getDefault(), "@" + tag + ": %s ", message));
+        }
+
+        @Override
+        public void i(String tag, String message) {
+            System.out.println(String.format(Locale.getDefault(), "@" + tag + ": %s ", message));
+        }
+
+        @Override
+        public void v(String tag, String message) {
+            System.out.println(String.format(Locale.getDefault(), "@" + tag + ": %s ", message));
+        }
+
+        @Override
+        public void wtf(String tag, String message) {
+            System.out.println(String.format(Locale.getDefault(), "@" + tag + ": %s ", message));
+
+        }
     }
 }
