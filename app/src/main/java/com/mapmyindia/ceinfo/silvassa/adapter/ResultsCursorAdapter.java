aaaa0da29f5615f81ea007202714de676e9aa246
Manujay K. Yadav <manujay1989@gmail.com>
2017-04-06 11:20:03 +0530

    add mulitline textview on results

--- app/src/main/java/com/mapmyindia/ceinfo/silvassa/adapter/ResultsCursorAdapter.java
+++ app/src/main/java/com/mapmyindia/ceinfo/silvassa/adapter/ResultsCursorAdapter.java
@@ -1,6 +1,7 @@
 package com.mapmyindia.ceinfo.silvassa.adapter;
 
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import com.mapmyindia.ceinfo.silvassa.R;
 import com.mapmyindia.ceinfo.silvassa.provider.property.PropertyCursor;
+import com.mapmyindia.ceinfo.silvassa.utils.StringUtils;
 
 import java.util.Locale;
 
@@ -33,7 +35,26 @@ public class ResultsCursorAdapter extends CursorRecyclerAdapter<RecyclerView.Vie
     @Override
     public void onBindViewHolderCursor(RecyclerView.ViewHolder holder, Cursor cursor) {
         PropertyCursor propertyCursor = new PropertyCursor(cursor);
-        ((ViewHolder) holder).mItemTextView.setText(String.format(Locale.getDefault(), "Property ID: %s \nOwner : %s", propertyCursor.getPropertyuniqueid(), propertyCursor.getPropertyowner()));
+        ((ViewHolder) holder).mItemTextView0.setText(
+                String.format(Locale.getDefault(),
+                        "Property ID : %-22s ",
+                        propertyCursor.getPropertyuniqueid()));
+        ((ViewHolder) holder).mItemTextView0.setTypeface(Typeface.MONOSPACE);
+        holder.itemView.setTag(propertyCursor.getPropertyuniqueid());
+        ((ViewHolder) holder).mItemTextView1.setText(
+                String.format(Locale.getDefault(), "Owner : %-22s ",
+                        propertyCursor.getPropertyowner()));
+        ((ViewHolder) holder).mItemTextView1.setTypeface(Typeface.MONOSPACE);
+        holder.itemView.setTag(propertyCursor.getPropertyuniqueid());
+        ((ViewHolder) holder).mItemTextView2.setText(
+                String.format(Locale.getDefault(), "Occupier : %-22s ",
+                        propertyCursor.getPropertyoccupiername()));
+        ((ViewHolder) holder).mItemTextView2.setTypeface(Typeface.MONOSPACE);
+        holder.itemView.setTag(propertyCursor.getPropertyuniqueid());
+        ((ViewHolder) holder).mItemTextView3.setText(
+                String.format(Locale.getDefault(), "HouseNo/Building : %-22s ",
+                        !StringUtils.isNullOrEmpty(propertyCursor.getPropertyhouseno()) ? propertyCursor.getPropertyhouseno() : propertyCursor.getPropertybuildingname()));
+        ((ViewHolder) holder).mItemTextView3.setTypeface(Typeface.MONOSPACE);
         holder.itemView.setTag(propertyCursor.getPropertyuniqueid());
     }
 
@@ -45,11 +66,14 @@ public class ResultsCursorAdapter extends CursorRecyclerAdapter<RecyclerView.Vie
 
     private class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView mItemTextView;
+        TextView mItemTextView0, mItemTextView1, mItemTextView2, mItemTextView3;
 
         private ViewHolder(View itemView) {
             super(itemView);
-            mItemTextView = (TextView) itemView.findViewById(R.id.list_item_results);
+            mItemTextView0 = (TextView) itemView.findViewById(R.id.list_item_results0);
+            mItemTextView1 = (TextView) itemView.findViewById(R.id.list_item_results1);
+            mItemTextView2 = (TextView) itemView.findViewById(R.id.list_item_results2);
+            mItemTextView3 = (TextView) itemView.findViewById(R.id.list_item_results3);
         }
     }
 }
